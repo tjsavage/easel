6a501d114685cee30146fb5eee70ec8fc007b02f
Taylor Savage <taylor@taylorsavage.com>
2013-12-10 21:22:44 -0800

    change db_setup in vincent to deal with key

--- vincent/db_setup.sql
+++ vincent/db_setup.sql
@@ -1,2 +1,2 @@
-CREATE TABLE IF NOT EXISTS doorbell_lock (status integer);
+CREATE TABLE IF NOT EXISTS doorbell_lock (status integer PRIMARY KEY);
 INSERT OR REPLACE INTO doorbell_lock (status) values (1);
