a576e9bd4d8213bd274efd227fe358d86a8b028d
Taylor Savage <taylor@taylorsavage.com>
2013-12-14 17:17:52 -0800

    tweaking

--- pablo/lib/animations.py
+++ pablo/lib/animations.py
@@ -151,9 +151,11 @@ class SunriseAnimation(KeyFrameAnimation):
 		self.config["keyFrames"] = []
 		self.config["keyFrames"].append(KeyFrame((0,0,0), 0.00))
 		self.config["keyFrames"].append(KeyFrame((0.09,0.04,0.17), 0.20))
-		self.config["keyFrames"].append(KeyFrame((0.25,0.02,0.25), 0.40))
-		self.config["keyFrames"].append(KeyFrame((0.32,0.15,0.15), 0.60))
-		self.config["keyFrames"].append(KeyFrame((0.46,0.35,0.19), 0.70))
+		self.config["keyFrames"].append(KeyFrame((0.15,0.01,0.15), 0.40))
+		self.config["keyFrames"].append(KeyFrame((0.16,0.08,0.08), 0.60))
+		self.config["keyFrames"].append(KeyFrame((0.26,0.17,0.19), 0.70))
 		self.config["keyFrames"].append(KeyFrame((0.92,0.85,0.33), 0.80))
 		self.config["keyFrames"].append(KeyFrame((1.0,0.96,0.85), 0.90))
+
+		self.config["loop"] = False
 
