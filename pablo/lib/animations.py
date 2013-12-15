bd88b46b27bf2fe1e8f7827b6cab4039ba5abb7f
Taylor Savage <taylor@taylorsavage.com>
2013-12-14 16:56:23 -0800

    tweaking

--- pablo/lib/animations.py
+++ pablo/lib/animations.py
@@ -1,5 +1,10 @@
 import math
 
+class KeyFrame():
+	def __init__(self, rgb, timing):
+		self.rgb = rgb
+		self.timing = timing
+
 class Animation():
 	def __init__(self, config=None):
 		self.config = config
@@ -14,6 +19,12 @@ class Animation():
 
 	def multiplyRGB(self, rgb, amplitude):
 		return (rgb[0] * amplitude, rgb[1] * amplitude, rgb[2] * amplitude)
+
+	def getFilledFrame(self, rgb):
+		buffer = []
+		for i in range(self.config["pixels"]):
+			buffer.append(rgb)
+		return buffer
 
 class PulseAnimation(Animation):
 	def __init__(self, config=None):
@@ -70,7 +81,6 @@ class RunnerAnimation(Animation):
 
 		headLocation = (frame * self.config["speed"]["step"]) % self.config["pixels"]
 		tailLocation = (headLocation - self.config["tail"]) % self.config["pixels"]
-		print "Head: %d, Tail: %d" % (headLocation, tailLocation)
 		for i in range(self.config["pixels"]):
 			if i > tailLocation and i <= headLocation:
 				amplitude = (self.config["tail"] - (headLocation - i)) / self.config["tail"] * 1.0
@@ -83,9 +93,41 @@ class RunnerAnimation(Animation):
 				buffer.append(self.multiplyRGB(self.config["color"], amplitude))
 			else:
 				buffer.append(self.config["backgroundColor"])
-		print buffer
-		print "-----------"
 		return buffer
+
+class KeyFrameAnimation(Animation):
+	def __init__(self, config=None):
+		Animation.__ini__(self, config)
+
+		if "keyFrames" not in self.config:
+			self.config["keyFrames"] = []
+			self.config["keyFrames"].append(KeyFrame((1,0,0), 0.15))
+			self.config["keyFrames"].append(KeyFrame((1,1,0), 0.30))
+			self.config["keyFrames"].append(KeyFrame((0,1,0), 0.45))
+			self.config["keyFrames"].append(KeyFrame((0,1,1), 0.60))
+			self.config["keyFrames"].append(KeyFrame((0,0,1), 0.75))
+			self.config["keyFrames"].append(KeyFrame((1,0,1), 0.90))
+
+	def getFrame(self, frame):
+		timing = ((frame * self.config["speed"]["step"]) % self.config["speed"]["max"]) / self.config["speed"]["max"] * 1.0
+		prevFrame = None
+		nextFrame = None
+		for keyFrame in self.config["keyFrames"]:
+			if keyFrame.timing < timing:
+				if prevFrame:
+					nextFrame = keyFrame
+					break
+				prevFrame = keyFrame
+		if not nextFrame:
+			nextFrame = self.config["keyFrames"][0]
+
+		interpolationRatio =  (timing - prevFrame.timing) / (nextFrame.timing - prevFrame.timing)
+		r = (nextFrame.rgb[0] - prevFrame.rgb[0]) * interpolationRatio + prevFrame.rgb[0]
+		g = (nextFrame.rgb[1] - prevFrame.rgb[1]) * interpolationRatio + prevFrame.rgb[1]
+		b = (nextFrame.rgb[2] - prevFrame.rgb[2]) * interpolationRatio + prevFrame.rgb[2]
+
+		return self.getFilledFrame((r, g, b))
+
 
 
 
