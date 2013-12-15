79f020e4032638cab678ca1cc0dbc8427ede7844
Taylor Savage <taylor@taylorsavage.com>
2013-12-14 17:11:50 -0800

    tweaking

--- pablo/lib/animations.py
+++ pablo/lib/animations.py
@@ -146,5 +146,5 @@ class SunriseAnimation(KeyFrameAnimation):
 		self.config["keyFrames"].append(KeyFrame((0.62,0.25,0.25), 0.60))
 		self.config["keyFrames"].append(KeyFrame((0.76,0.45,0.25), 0.70))
 		self.config["keyFrames"].append(KeyFrame((0.92,0.85,0.33), 0.80))
-		self.config["keyFrames"].append(KeyFrame((1.0,0.96,0.85), 0.40))
+		self.config["keyFrames"].append(KeyFrame((1.0,0.96,0.85), 0.90))
 
