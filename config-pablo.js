e0849c701a41335f2d2100ce183921d46374acc0
Taylor Savage <taylor@taylorsavage.com>
2013-12-29 20:21:37 -0800

    added motionDetector code

--- config-pablo.js
+++ config-pablo.js
@@ -20,6 +20,25 @@ var config = {
 					}
 				}
 			}
+		},
+		{
+			"type": "motion_detector",
+			"options": {
+				"name": "bed_motion_detector",
+				"pin": 18,
+				"skynet": {
+					"me": {
+						"type": "client",
+						"name": "bed_motion_detector",
+						"port": 3003
+					},
+					"server": {
+						"name": "skynet-server",
+						"port": 3001,
+						"ip": "10.0.1.19"
+					}
+				}
+			}
 		}
 	]
 };
--- modules/motion_detector/index.js
+++ modules/motion_detector/index.js
@@ -3,22 +3,33 @@ var gpio = require("gpio");
 var motionDetector = function(app, options) {
 	this.options = options;
 	this.skynet = new Skynet(this, options.skynet);
+	this.state = {
+		"tripped": false,
+		"lastTripped": null
+	};
 
 	this.gpioPin = gpio.export(options.pin, {
 		direction: "in",
 		ready: function() {
+			console.log("motion detector ready");
 		}
 	});
 
+	var T = this;
+
 	this.gpioPin.on("change", function(val) {
 		console.log("changed motion detector pin to " + val);
-		onMotion();
+		if (val) {
+			T.state.tripped = true;
+			T.state.lastTripped = (new Date()).toString();
+		} else {
+			T.state.tripped = false;
+		}
+		T.skynet.emitState(T.state);
 	});
 
-	function onMotion() {
-		this.skynet.emitMessage({
-			"from": this.options.name,
-			"body": "motion"
-		});
-	}
+
+	this.skynet.onGetState(function() {
+		return T.state;
+	});
 };
