af26038674d2cc81c3e50ef61ad914876abc4b7d
Taylor Savage <taylor@taylorsavage.com>
2013-12-29 21:40:17 -0800

    making pulse animation have max and min values

--- modules/led_strip/animations/pulse.js
+++ modules/led_strip/animations/pulse.js
@@ -5,17 +5,26 @@ var STEPS = 100;
 
 function Pulse(setter, duration, options) {
 	Animation.call(this, setter, duration, options);
-};
+
+	this.minValue = 0;
+	this.maxValue = 1;
+	if (typeof options.minValue != "undefined") {
+		this.minValue = options.minValue;
+	}
+	if (typeof options.maxValue != "undefined") {
+		this.maxValue = options.maxValue;
+	}
+}
 util.inherits(Pulse, Animation);
 
 Pulse.prototype.refresh = function(percentComplete) {
 	var angle = (Math.PI * 2) * percentComplete;
-	var value = (Math.sin(angle) + 1.0) / 2.0;
+	var value = (Math.sin(angle) + 1.0 + this.minValue) / (2.0) * (this.maxValue - this.minValue);
 
 	this.setter.setValue(value);
 	this.setter.emitState();
 	this.setter.all();
-}
+};
 
 
 module.exports = Pulse;
