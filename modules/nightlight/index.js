a88903f8064bb48b7f2e19211a9c26ba0d6f0db4
Taylor Savage <taylor@taylorsavage.com>
2014-01-19 15:27:10 -0800

    made nightlight only turn on if the strip was off

--- modules/nightlight/index.js
+++ modules/nightlight/index.js
@@ -17,32 +17,30 @@ var nightlight = function(app, options) {
 	this.skynet.onBroadcastState(this.options.motionDetector, function(stateData) {
 		if (stateData.tripped) {
 			T.tripped = true;
-			this.skynet.setState(T.options.ledStrip, {
-				"power": true,
-				"color": {
-					"h": 338,
-					"s": 0.34,
-					"v": 0.93,
-				},
-				"animation": {
-					"name": "pulse",
-					"duration": 3000,
-					"options": {
-						"loop": true,
-						"minValue": 0.3,
-						"maxValue": 0.6
+			if (!T.priorLedState.power) {
+				this.skynet.setState(T.options.ledStrip, {
+					"power": true,
+					"color": {
+						"h": 338,
+						"s": 0.34,
+						"v": 0.93,
+					},
+					"animation": {
+						"name": "pulse",
+						"duration": 3000,
+						"options": {
+							"loop": true,
+							"minValue": 0.3,
+							"maxValue": 0.6
+						}
 					}
-				}
-			});
+				});
+			}
 		} else {
 			T.tripped = false;
-			if (typeof T.priorLedState == 'undefined' || !T.priorLedState) {
-				T.priorLedState = {
-					power: false
-				};
-			}
-			console.log("priorState",T.priorLedState);
-			this.skynet.setState(T.options.ledStrip, T.priorLedState);
+			this.skynet.setState(T.options.ledStrip, {
+				power: false
+			});
 		}
 	});
 };
